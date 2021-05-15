b7bafc49f87b53bcb05464fb448573a989838bbc
kimilg <ilgoo@naver.com>
2021-05-15 23:43:43 +0900

    second commit

--- index.php
+++ index.php
@@ -8,24 +8,24 @@
 <html>
 <head>
  <meta charset="utf-8">
- <link rel="stylesheet" type="text/css" href="http://localhost/style.css">
+ <link rel="stylesheet" type="text/css" href="/style.css">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
- <link href="http://localhost/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+ <link href="/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body id="target">
   <div class="container">
    <header class="jumbotron text-center">
     <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" class="img-circle" id="logo" />
-    <h1><a href="http://localhost/index.php">JavaScript</a></h1>
+    <h1><a href="/index.php">JavaScript</a></h1>
    </header>
   <div class="row">
     <nav class="col-md-3">
      <ol class="nav nav-pills nav-stacked">
        <?php
          while($row = mysqli_fetch_assoc($result)){
-           echo '<li><a href="http://localhost/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+           echo '<li><a href="/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
          }
        ?>
      </ol>
@@ -49,12 +49,12 @@
           <input type="button" value="white" onclick="document.getElementById('target').className='white'" class="btn btn-default btn-lg" />
           <input type="button" value="black" onclick="document.getElementById('target').className='black'" class="btn btn-default btn-lg" />
         </div>
-        <a href="http://localhost/write.php" class="btn btn-success btn-lg">write</a>
+        <a href="/write.php" class="btn btn-success btn-lg">write</a>
       </div>
   </div>
 </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script src="http://localhost/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+<script src="/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
